f766e2e0247f27865ecab2efa818dffd719e5879
carsoncall <carsonrc@github.com>
2023-12-14 21:35:34 -0700

    create works!

--- src/create/create.jsx
+++ src/create/create.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Row, Col } from 'react-bootstrap';
 import { themes } from '../assets/themes.js'
 //hostname for backend -- debugging purposes
 import hostname from '../assets/hostname.js';
@@ -14,6 +14,7 @@ const Canvas = React.forwardRef(({ }, ref) => {
 });
 
 function ImageButtons({ aesthetic, imageCanvas, canvasContext }) {
+
   const [loading, setLoading] = React.useState(false);
   const filePicker = React.useRef(null);
 
@@ -24,27 +25,26 @@ function ImageButtons({ aesthetic, imageCanvas, canvasContext }) {
 
   async function convertImageHandler() {
     console.log("convert image clicked");
-    if (aesthetic.paletteColors.length === 0) {
+    if (!aesthetic.current.paletteColors) {
       alert("No theme selected! Select a theme and try again.");
       console.log("No theme selected! Select a theme and try again.");
-      return;
     } else {
       setLoading(true);
-      let imageData = canvasContext.getImageData(0, 0, imageCanvas.width, imageCanvas.height);
-      await aesthetic.convertImage(imageData, imageCanvas, canvasContext);
+      let imageData = canvasContext.current.getImageData(0, 0, imageCanvas.current.width, imageCanvas.current.height);
+      await aesthetic.current.convertImage(imageData, imageCanvas.current, canvasContext.current);
       setLoading(false);
     }
   }
 
   function downloadImageHandler() {
     console.log("download image clicked");
-    if (aesthetic._aestheticImage.src === "") {
+    if (aesthetic.current._aestheticImage.src === "") {
       alert("No image to download! Convert an image and try again.");
       console.log("No image to download! Convert an image and try again.");
       return;
     } else {
       let downloadLink = document.createElement("a");
-      downloadLink.href = aesthetic._aestheticImage.src;
+      downloadLink.href = aesthetic.current._aestheticImage.src;
       downloadLink.download = "aesthetic.png";
       downloadLink.click();
     }
@@ -52,7 +52,20 @@ function ImageButtons({ aesthetic, imageCanvas, canvasContext }) {
 
   function undoChangesHandler() {
     console.log("undo changes clicked");
-    currentAesthetic.loadImage("originalImage", imageCanvas, canvasContext);
+    aesthetic.current.loadImage(aesthetic.current._originalImage.src, "originalImage", imageCanvas.current, canvasContext.current);
+  }
+
+  function handleFilePickerChange(event) {
+    let file = event.target.files[0];
+    if (file) {
+      let reader = new FileReader();
+      reader.onloadend = () => {
+        aesthetic.current.loadImage(reader.result, "aestheticImage", imageCanvas.current, canvasContext.current);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      console.log("No file selected");
+    }
   }
 
   return (
@@ -60,7 +73,7 @@ function ImageButtons({ aesthetic, imageCanvas, canvasContext }) {
       {loading ? <span>Loading...</span> :
         <div id="image-buttons" >
           <Button variant="success" id="upload-image" onClick={uploadImageHandler} className='m-2'>Upload Image</Button>
-          <input ref={filePicker} type="file" id="upload-image-selector" accept=".jpg, .png" style={{ "display": "none" }} />
+          <input ref={filePicker} type="file" id="upload-image-selector" onChange={handleFilePickerChange} accept=".jpg, .png" style={{ "display": "none" }} />
           <Button variant="success" id="convert-image" onClick={convertImageHandler} className='m-2'>Convert Image</Button>
           <Button variant="success" id="download-image" onClick={downloadImageHandler} className='m-2'>Download Image</Button>
           <Button variant="success" id="undo-changes" onClick={undoChangesHandler} className='m-2'>Undo Changes</Button>
@@ -71,15 +84,21 @@ function ImageButtons({ aesthetic, imageCanvas, canvasContext }) {
   )
 }
 
-function Themes() {
-  const [theme, setTheme] = React.useState("gruvbox");
+function Themes({ aesthetic }) {
+  const [theme, setTheme] = React.useState("");
   const [displayPicker, setDisplayPicker] = React.useState(false);
+  const [paletteColors, setPaletteColors] = React.useState([]);
 
   useEffect(() => {
     if (theme === "custom") {
       setDisplayPicker(true);
+      //TODO: support file uploading
     } else {
+      aesthetic.current.paletteName = theme;
+      aesthetic.current.paletteColors = themes[theme];
+      setPaletteColors(themes[theme]);
       setDisplayPicker(false);
+      console.log("Theme selected: ", theme);
     }
   }, [theme]);
 
@@ -88,7 +107,7 @@ function Themes() {
       <legend>
         <h5>Themes</h5>
       </legend>
-      <ToggleButtonGroup type="radio" name="options" defaultValue="gruvbox" onChange={setTheme}>
+      <ToggleButtonGroup type="radio" name="options" onChange={setTheme}>
         <ToggleButton id="gruvbox" value="gruvbox">gruvbox</ToggleButton>
         <ToggleButton id="solarized" value="solarized">solarized</ToggleButton>
         <ToggleButton id="catpuccin" value="catpuccin">catpuccin</ToggleButton>
@@ -100,21 +119,16 @@ function Themes() {
         <div id="custom-theme-selector-wrapper">
           <input type="file" id="custom-theme-selector" accept=".json" />
         </div> : null}
+      <h5>Palette </h5>
+      {aesthetic.current.paletteColors ?
+        <Row>
+          {aesthetic.current.paletteColors.map((color, index) => (
+            <Col key={index} style={{ backgroundColor: `rgb(${color[0]}, ${color[1]}, ${color[2]})`, height: '50px' }} />
+          ))}
+        </Row> : null}
     </>
   )
 }
-
-function Palette() {
-  return (
-    <>
-      <div id="palette-title">
-        <h5>Palette </h5>
-      </div>
-      <div id="palette"></div>
-    </>
-  );
-}
-
 
 export default function Create() {
   const aesthetic = React.useRef(new Aesthetic());
@@ -135,28 +149,53 @@ export default function Create() {
     loadRandomURL();
   }, []);
 
+  function setTitle(event) {
+    aesthetic.current.title = event.target.value;
+  }
+
+  function uploadAestheticHandler() {
+    if (!aesthetic.current.title) {
+      alert("You must give your aesthetic a title!");
+      return;
+    }
+    if (sessionStorage.getItem('sessionToken')) {
+      uploadAesthetic(aesthetic)
+        .then(response => {
+          console.log(response);
+          if (response["result"] === "success") {
+            alert("Aesthetic successfully uploaded!");
+          } else {
+            alert("Something went wrong with uploading the aesthetic");
+            throw new Error("Something went wrong with uploading the aesthetic");
+          }
+        })
+        .catch(error => {
+          console.error("Error uploading aesthetic: ", error);
+        })
+    } else {
+      alert("You must be logged in to share an aesthetic!");
+    }
+  }
+
 
   return (
     <div className='container-fluid vh-100 bg-secondary text-center'>
       <div>
         <h5>Upload an image (or use the provided random one), choose a theme, and create an Aesthetic</h5>
       </div>
-      <div id="aesthetic-title">
+      <div id="aesthetic-title" className='d-flex flex-column align-items-center'>
         <Form.Label htmlFor="aesthetic-title-input">Aesthetic Title: </Form.Label>
-        <Form.Control type="text" id="aesthetic-title-input" defaultValue="Untitled" />
+        <Form.Control type="text" onChange={setTitle} id="aesthetic-title-input" defaultValue="Untitled" style={{"width":"500px"}}/>
       </div>
       <div id="image-wrapper">
         <Canvas ref={imageCanvas} />
       </div>
-      <ImageButtons aesthetic={aesthetic.current} imageCanvas={imageCanvas.current} canvasContext={canvasContext.current} />
+      <ImageButtons aesthetic={aesthetic} imageCanvas={imageCanvas} canvasContext={canvasContext} />
       <div>
         <Themes aesthetic={aesthetic} />
         <br />
         <br />
-        <Palette aesthetic={aesthetic} />
-        <br />
-        <br />
-        <Button variant="success" id="create-aesthetic" className='m-2'>Create Aesthetic</Button>
+        <Button variant="success" onClick={uploadAestheticHandler} id="share-aesthetic" className='m-2'>Share Aesthetic</Button>
         <br />
         <br />
         <br />
@@ -187,8 +226,8 @@ async function fetchRandomImageURL() {
     })
 }
 
-async function uploadAesthetic() {
-  let aestheticJSONString = JSON.stringify(currentAesthetic);
+async function uploadAesthetic(aesthetic) {
+  let aestheticJSONString = JSON.stringify(aesthetic.current);
   console.log("Uploading the following JSON:", aestheticJSONString);
   return await fetch(`${hostname}/upload-aesthetic`, {
     method: 'PUT',
@@ -206,9 +245,11 @@ async function uploadAesthetic() {
     })
     .then(data => {
       if (data["result"] === "success") {
-        alert("Aesthetic successfully uploaded!");
+        console.log("Aesthetic successfully uploaded!");
+        return data;
       } else if (data["result"] === "error") {
-        alert(`The server sent back the following error: ${data["error"]}`);
+        console.log("Error uploading aesthetic: ", data["error"]);
+        return data;
       }
     })
     .catch(error => {
